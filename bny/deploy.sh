6c58412bfffd8612f74c475dc1d9c24930057421
cnzixn <cnzixn@gmail.com>
2024-11-13 05:51:44 +0800

    Deploy

--- bny/deploy.sh
+++ bny/deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 网站名字 xxx.com/bny
-web_name="bny"
-
 # cd /sdcard/acode/bny.otfo.cc/
 # bash deploy.sh
 
@@ -19,15 +16,27 @@ git rm -rf .
 
 # 将 _site 文件夹内容复制到 gh-pages 分支根目录
 git checkout main -- _site
-git checkout main -- .gitignore
 git checkout main -- search.json
 cp -r ./_site/s .
-mv _site "$web_name"
+mv _site bny
+mv ./bny/index.html .
+mv ./bny/404.html .
 
 # 提交并推送到 gh-pages 分支
 git add .
+git reset deploy.sh
 git commit -m "Deploy"
 git push origin gh-pages --force
 
 # 返回到主分支
 git checkout main
+
+
+
+
+
+# jekyll build
+
+# jekyll serve
+
+# git remote add origin https://github.com/cnzixn/bny.otfo.cc.git

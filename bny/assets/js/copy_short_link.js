e04229b3cc6b67b9114111400a09cd6d7a61f083
cnzixn <cnzixn@gmail.com>
2024-11-13 06:38:39 +0800

    Deploy

---
+++ bny/assets/js/copy_short_link.js
@@ -0,0 +1,6 @@
+function copyShortLink() {
+  var shortLink = document.getElementById("shortLink");
+  shortLink.select();  // 选择文本
+  document.execCommand("copy");  // 执行复制操作
+  // alert("短链接已复制: " + shortLink.value);  // 弹出提示框，显示已复制的短链接
+}
